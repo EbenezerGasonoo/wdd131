c113c7673d351f2ff9a6a3d77c25e8b2bb658d69
Ebenezer Gasonoo <gas24001@byui.edu>
2024-12-01 01:41:34 +0000

    fixes

--- scripts/temples.js
+++ scripts/temples.js
@@ -4,16 +4,15 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = document.lastModified;
 
-// Get the hamburger button and the navigation menu
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('nav ul');
 
-// Add event listener to toggle visibility
+
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('visible');  // Toggle the "visible" class
+    menu.classList.toggle('visible');  
     if (menu.classList.contains('visible')) {
-        hamburger.innerHTML = 'X';  // Change to "X" when menu is open
-    } else {
-        hamburger.innerHTML = '&#9776;';  // Change back to hamburger icon when menu is closed
+        hamburger.innerHTML = 'X';  
+        hamburger.innerHTML = '&#9776;';  
     }
 });
